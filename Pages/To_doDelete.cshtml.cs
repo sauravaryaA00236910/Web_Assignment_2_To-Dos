086af2961827c6c1fd56765a32bd05b9722a7e4c
sauravarya <A00236910@mycambrian.ca>
2021-07-30 03:48:59 +0530

    Delete functionality added for the To Do table

---
+++ Pages/To_doDelete.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web_Assignment_2_To_Dos.Data;
+using Web_Assignment_2_To_Dos.Data.Context;
+
+namespace Web_Assignment_2_To_Dos.Pages
+{
+    public class To_doDeleteModel : PageModel
+    {
+        To_DoContext _context;
+
+        [FromQuery]
+        public To_Do UpdateTo_do { get; set; }
+
+        public To_doDeleteModel(To_DoContext context)
+        {
+            _context = context;
+        }
+
+        public RedirectToPageResult OnGet()
+        {
+            if(UpdateTo_do.To_DoItem != null)
+            {
+                _context.To_Dos.Remove(UpdateTo_do);
+                _context.SaveChanges();
+
+            }
+            return new RedirectToPageResult("Index");
+        }
+    }
+}
